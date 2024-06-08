d7b7c66052913a952ff8e3cceaac53e69e85ba14
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-06-08 00:36:53 -0400

    fixed pathing

--- OverworldMap.js
+++ OverworldMap.js
@@ -20,13 +20,13 @@ class OverworldMap {
 
 window.OverworldMaps = {
     DemoRoom: {
-        lowerSrc: "/images/maps/DemoLower.png",
-        upperSrc: "/images/maps/DemoUpper.png",
+        lowerSrc: "./images/maps/DemoLower.png",
+        upperSrc: "./images/maps/DemoUpper.png",
         gameObjects: {
             // npc1: new Person({
             //     x: utils.withGrid(7),
             //     y: utils.withGrid(9),
-            //     src: "/images/characters/people/npc1.png"
+            //     src: "./images/characters/people/npc1.png"
             // }),
             hero: new Person ({
                 isPlayerControlled: true,
@@ -36,8 +36,8 @@ window.OverworldMaps = {
         }
     },
     Kitchen: {
-        lowerSrc: "/images/maps/KitchenLower.png",
-        upperSrc: "/images/maps/KitchenUpper.png",
+        lowerSrc: "./images/maps/KitchenLower.png",
+        upperSrc: "./images/maps/KitchenUpper.png",
         gameObjects: {
             hero: new GameObject ({
                 x: 3,
@@ -46,12 +46,12 @@ window.OverworldMaps = {
             npcA: new GameObject({
                 x: 9,
                 y: 6,
-                src: "/images/characters/people/npc2.png"
+                src: "./images/characters/people/npc2.png"
             }),
             npcB: new GameObject({
                 x: 10,
                 y: 8,
-                src: "/images/characters/people/npc3.png"
+                src: "./images/characters/people/npc3.png"
             }),
         }
     }
