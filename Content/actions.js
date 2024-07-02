b1e588b7f9ce2465c147ab65766fdfde39b52031
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-07-02 15:06:23 -0400

    added item menu and submission

--- Content/actions.js
+++ Content/actions.js
@@ -1,6 +1,7 @@
 window.Actions = {
   damage1: {
     name: "Smack!",
+    description: "Smack dough across their face",
     success: [
       { type: "textMessage", text: "{CASTER} used {ACTION}!" },
       { type: "animation", animation: "spin" },
@@ -9,6 +10,7 @@ window.Actions = {
   },
   saucyStatus: {
     name: "Tomato Squeeze!",
+    description: "Covers you in sauce",
     targetType: "friendly",
     success: [
       { type: "textMessage", text: "{CASTER} used {ACTION}!" },
@@ -18,20 +20,34 @@ window.Actions = {
   },
   clumsyStatus: {
     name: "Olive oil",
+    description: "Slippery and Dangerous",
     targetType: "enemy",
     success: [
       { type: "textMessage", text: "{CASTER} used {ACTION}!" },
-      {type: "animation", animation: "glob", color: "#dafd2a"},
+      { type: "animation", animation: "glob", color: "#dafd2a" },
       { type: "stateChange", status: { type: "clumsy", expires: 3 } },
       { type: "textMessage", text: "{TARGET} became clumsy!" },
-
     ],
   },
   saucyStatus2: {
     name: "SUPER TOMATO SQUEEEEZE!!!!!",
+    description: "Super Powerful Tomato squeeze",
     success: [
       { type: "textMessage", text: "{CASTER} used {ACTION}!" },
       { type: "stateChange", status: { type: "saucy", expires: 3 } },
     ],
   },
+  item_recoverStatus: {
+    name: "Heating Lamp",
+    description: "Feeling fresh and warm",
+    targetType: "friendly",
+    success: [
+      { type: "textMessage", text: "{CASTER} used a {ACTION}!" },
+      { type: "stateChange", status: null },
+      { type: "textMessage", text: "{CASTER} is all warmed up!" },
+
+    ],
+  },
 };
+
+// Items
