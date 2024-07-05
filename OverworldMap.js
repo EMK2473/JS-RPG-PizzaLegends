a6e504f9e57348b2f7f2e4be1f0f798a8be4d7d8
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-07-05 11:43:33 -0400

    added crafting object and crafting menu/ui

--- OverworldMap.js
+++ OverworldMap.js
@@ -203,6 +203,12 @@ window.OverworldMaps = {
         //   { type: "walk", direction: "down" },
         // ],
       }),
+      pizzaStone: new PizzaStone({
+        x: utils.withGrid(2),
+        y: utils.withGrid(7),
+        storyFlag: "USED_PIZZA_STONE",
+        pizzas: ["v001", "c001"]
+      })
     },
     walls: {
       // table
