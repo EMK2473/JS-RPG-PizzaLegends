6d82e09ebd6f89f51a5c5a9ff49156e0bf1be314
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-07-20 15:11:30 -0400

    modified buttons.js

--- Buttons.js
+++ Buttons.js
@@ -4,26 +4,33 @@ class Buttons {
     }
 
     createElement() {
+        // create container for buttons
         this.element = document.createElement("div");
         this.element.classList.add("ButtonsContainer");
 
+        // create buttton directions for input values
         this.upButton = this.createButton("Up", "up");
         this.leftButton = this.createButton("Left", "left");
         this.rightButton = this.createButton("Right", "right");
         this.downButton = this.createButton("Down", "down");
         this.enterButton = this.createButton("Enter", "enter");
 
+        // append buttons to container
         this.element.appendChild(this.upButton);
         this.element.appendChild(this.leftButton);
         this.element.appendChild(this.enterButton);
         this.element.appendChild(this.rightButton);
         this.element.appendChild(this.downButton);
 
+        // append the container to the document
         document.body.appendChild(this.element);
 
+        // call method to handle setting up event listeners
         this.setupListeners();
     }
 
+    // Methods
+    // method to create a button
     createButton(label, action) {
         const button = document.createElement("button");
         button.innerText = label;
@@ -32,6 +39,7 @@ class Buttons {
         return button;
     }
 
+    // sets event listeners for clicking on buttons
     setupListeners() {
         this.element.addEventListener("click", (event) => {
             const action = event.target.dataset.action;
@@ -41,16 +49,19 @@ class Buttons {
         });
     }
 
+    // create object to simulate keydown press
     handleButtonClick(action) {
         const event = new KeyboardEvent("keydown", { code: this.getKeyCode(action) });
         document.dispatchEvent(event);
 
+    // create object to simulate keyup release with timing
         setTimeout(() => {
             const event = new KeyboardEvent("keyup", { code: this.getKeyCode(action) });
             document.dispatchEvent(event);
         }, 100); 
     }
 
+    // method to handle mapped buttons to KeyPressListeners
     getKeyCode(action) {
         const keyMap = {
             "up": "ArrowUp",
